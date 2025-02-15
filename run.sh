36b1db44a5af09815d28e1a7e863addf603c4647
yzamir <kobi.zamir@gmail.com>
2025-02-15 12:12:23 +0200

    hooks
    Signed-off-by: yzamir <kobi.zamir@gmail.com>

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ WORKERS=${WORKERS:-4}
 SSL_ENABLE=${SSL_ENABLE:-"false"}
 SSL_CERT=${SSL_CERT:-"certs/server.crt"}
 SSL_KEY=${SSL_KEY:-"certs/server.key"}
+HOOK_COMMAND=${HOOK_COMMAND:-""}
 
 # Base Gunicorn options
 GUNICORN_OPTS="--workers $WORKERS --bind 0.0.0.0:$PORT"
@@ -16,6 +17,13 @@ if [[ "$SSL_ENABLE" == "true" ]]; then
     GUNICORN_OPTS="$GUNICORN_OPTS --certfile=$SSL_CERT --keyfile=$SSL_KEY"
 else
     echo "Starting without SSL support"
+fi
+
+# Show hook status
+if [[ -n "$HOOK_COMMAND" ]]; then
+    echo "Hook command enabled: $HOOK_COMMAND"
+else
+    echo "No hook command configured"
 fi
 
 # Change the Gunicorn target from create_app to wsgi:app
